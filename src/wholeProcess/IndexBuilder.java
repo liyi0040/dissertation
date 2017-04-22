4f00dfa92b8fc53ed3a7aa67f1a39b50dad530f5
Y L <liying_211@163.com>
2017-04-22 08:09:40 +0800

    exe can be processed

--- src/wholeProcess/IndexBuilder.java
+++ src/wholeProcess/IndexBuilder.java
@@ -1,11 +1,15 @@
 package wholeProcess;
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.StringReader;
+import java.lang.reflect.Executable;
 import java.nio.file.FileSystems;
 
 import java.sql.Connection;
@@ -52,17 +56,28 @@ public class IndexBuilder {
 
 	static long startTime = System.currentTimeMillis();
 	static int nonZero =0;
+	
+	static String inputFile="triOutputA.txt";
+	
+	static ArrayList<String> markSe =new ArrayList<>();
+	static ArrayList<String> markCl =new ArrayList<>();
+	static int count1=0;
 /**
  * methods
  * @param args
  * @throws SQLException
  * @throws IOException
+ * @throws InterruptedException 
  */
-	public static void main(String[] args) throws SQLException, IOException {
+	public static void main(String[] args) throws SQLException, IOException, InterruptedException {
 
-		connAllData();
-		featureExtration();
-	}
+		//connAllData();
+		//featureExtration();
+		String outputfile =clustering();	
+		System.out.println("=====");
+    	buildFlag();
+    	cluster2(outputfile);
+    }
 
 
 	public static void  connAllData() throws IOException {
@@ -224,7 +239,7 @@ public class IndexBuilder {
 	public static void featureExtration() throws IOException{
 
 		//ÌØÕ÷¾ØÕó
-		File output = new File("triOutputA.txt");
+		File output = new File(inputFile);
 		output.createNewFile();
 		FileWriter outputWrite = new FileWriter(output);
         BufferedWriter bufferWritter = new BufferedWriter(outputWrite);
@@ -370,6 +385,341 @@ public class IndexBuilder {
 		        
 		
 	}
+	
+	public static String clustering() throws IOException, InterruptedException{
 
-}
+			String openApp = "cmd.exe /c  vcluster.exe";
+			String parameters ="";
+			String output = "-clustfile=2cluster3AT.txt";
+			String outputName ="2cluster3AT.txt";
+			String ClusterNum = "2";
+			String cmd= openApp+""+parameters+" "+output+" "+inputFile+" "+ClusterNum;
+			System.out.println(cmd);
+			Process process = Runtime.getRuntime().exec(cmd);
+			
+		    BufferedInputStream in = new BufferedInputStream(process.getInputStream());  
+		    BufferedReader inBr = new BufferedReader(new InputStreamReader(in));  
+		    String lineStr;  
+		    while ((lineStr = inBr.readLine()) != null)  
+		    System.out.println(lineStr);
+		    
+		    inBr.close();  
+		    in.close();  
+		    process.waitFor();
+
+			return outputName;  
+
+
+
+
+	}
+	
+
+    static void cluster2(String clusterRes){
+
+
+        int count1=0;
+        int count2=0;
+
+    	 try {
+    	    	FileReader fileReader = new FileReader("flag.txt");
+    	        BufferedReader bufferedReader = new BufferedReader(fileReader);
+    	        String line = null;
+    	        while ((line=bufferedReader.readLine())!= null) {
+    	        	count1++;
+    				markSe.add(line);
+    				
+    			}
+    		} catch (IOException e) {
+    			e.printStackTrace();
+    		}
+
+ /*
+  * result for 2 cluster
+  */
+    	 try{
+    		 System.out.println(clusterRes);
+    		 FileReader resultReader = new FileReader(clusterRes);
+ 	        BufferedReader bufferedReader = new BufferedReader(resultReader);
+ 	        String result;
+ 	        while ((result=bufferedReader.readLine())!= null) {
+ 				count2++;
+ 				markCl.add(result);
+ 			}
+    	 } catch (IOException e) {
+			// TODO: handle exception
+ 			e.printStackTrace();
+		}    
+
+		 int nn=0;
+		 int n0=0;
+		 int n1=0;
+		 int s0=0;
+		 int s1=0;
+		 int u0=0;
+		 int u1=0;
+		 int s2=0;
+		 int u2=0;
+				 
+    	 for(int i2 =0;i2<count2;i2++){
+    		String mk = markSe.get(i2);
+    		String res = markCl.get(i2);
+	        //System.out.println(mk);
+    		 if (res.equals("-1")) {
+				nn++;
+				if (mk.equals("selected")) {
+					s2++;
+				}else if(mk.equals("un")){
+					u2++;
+				}
+			}else if (res.equals("0")) {
+				n0++;
+				if (mk.equals("selected")) {
+					s0++;
+				}else if(mk.equals("un")){
+					u0++;
+				}
+			}else if (res.equals("1")) {
+				n1++;
+				if (mk.equals("selected")) {
+					s1++;
+				}else if(mk.equals("un")){
+					u1++;
+				}
+			}
+    	 }
+    	 System.out.println(nn+" "+n0+" "+n1);
+    	 System.out.println("s0: "+s0);
+    	 System.out.println("u0: "+u0);
+    	 System.out.println("s1: "+s1);
+    	 System.out.println("u1: "+u1);
+    	 System.out.println(s0);
+    	 System.out.println(s1);
+    	 System.out.println(u0);
+    	 System.out.println(u1);
+    	 System.out.println(s2);
+    	 System.out.println(u2);
+    	 
+    	 System.out.println("0: "+  (float)s0/(float)(s0+u0));
+    	 System.out.println("1: "+  (float)s1/(float)(s1+u1));
+    	 System.out.println("1: "+  (float)s0/(float)(s0+s1));
+
+
+
+
+
+ 	    	   
+    	    	     	 
+//    	 System.out.println("1: "+count2);
+//    	 System.out.println("2: "+markCl.size());
+
+	
+    }
+
+    static void cluster3(){
+        int count2=0;
+ /*
+  * result for 3 cluster
+  */
+    	 try{
+    		 FileReader resultReader = new FileReader("rbrAB3");
+ 	        BufferedReader bufferedReader = new BufferedReader(resultReader);
+ 	        String result;
+ 	        while ((result=bufferedReader.readLine())!= null) {
+ 				count2++;
+ 				markCl.add(result);
+ 			}
+    	 } catch (IOException e) {
+			// TODO: handle exception
+ 			e.printStackTrace();
+		}    
+
+    	 //cluster tag:-1, 0,1,2
+		 int nn=0;
+		 int n0=0;
+		 int n1=0;
+		 int n2=0;
+		 
+		 //count
+		 int s0=0;
+		 int u0=0;
+		 int s1=0;
+		 int u1=0;
+		 int s2=0;
+		 int u2=0;
+				 
+    	 for(int i2 =0;i2<count2;i2++){
+    		String mk = markSe.get(i2);
+    		String res = markCl.get(i2);
+	        System.out.println(mk);
+    		 if (res.equals("-1")) {
+				nn++;
+			}else if (res.equals("0")) {
+				n0++;
+				if (mk.equals("selected")) {
+					s0++;
+				}else if(mk.equals("un")){
+					u0++;
+				}
+			}else if (res.equals("1")) {
+				n1++;
+				if (mk.equals("selected")) {
+					s1++;
+				}else if(mk.equals("un")){
+					u1++;
+				}
+			}else if (res.equals("2")) {
+				n1++;
+				if (mk.equals("selected")) {
+					s2++;
+				}else if(mk.equals("un")){
+					u2++;
+				}
+			}
+    	 }
+    	 System.out.println(nn+" "+n0+" "+n1);
+//    	 System.out.println("s0: "+s0);
+//    	 System.out.println("u0: "+u0);
+//    	 System.out.println("s1: "+s1);
+//    	 System.out.println("u1: "+u1);
+    	 System.out.println(s0);
+    	 System.out.println(s1);
+    	 System.out.println(s2);
+    	 System.out.println(u0);
+    	 System.out.println(u1);
+    	 System.out.println(u2);
+    	 
+    	 System.out.println("0: "+  (float)s0/(float)(s0+u0));
+    	 System.out.println("1: "+  (float)s1/(float)(s1+u1));
+    	 System.out.println("1: "+  (float)s0/(float)(s0+s1));
+
+
+
+
+
+ 	    	   
+    	    	     	 
+    	 System.out.println("1: "+count2);
+//    	 System.out.println("2: "+markCl.size());
+
+	
+    }
+
+    static void cluster4(){
+        int count2=0;
+ /*
+  * result for 4 cluster
+  */
+    	 try{
+    		 FileReader resultReader = new FileReader("rbrAh2A4.txt");
+ 	        BufferedReader bufferedReader = new BufferedReader(resultReader);
+ 	        String result;
+ 	        while ((result=bufferedReader.readLine())!= null) {
+ 				count2++;
+ 				markCl.add(result);
+ 			}
+    	 } catch (IOException e) {
+			// TODO: handle exception
+ 			e.printStackTrace();
+		}    
+
+    	 //cluster tag:-1, 0,1,2,3
+		 int nn=0;
+		 int n0=0;
+		 int n1=0;
+		 int n2=0;
+		 int n3=0;
+
+		 
+		 //count
+		 int s0=0;
+		 int u0=0;
+		 int s1=0;
+		 int u1=0;
+		 int s2=0;
+		 int u2=0;
+		 int s3=0;
+		 int u3=0;
+				 
+    	 for(int i2 =0;i2<count2;i2++){
+    		String mk = markSe.get(i2);
+    		String res = markCl.get(i2);
+	        System.out.println(mk);
+    		 if (res.equals("-1")) {
+				nn++;
+			}else if (res.equals("0")) {
+				n0++;
+				if (mk.equals("selected")) {
+					s0++;
+				}else if(mk.equals("un")){
+					u0++;
+				}
+			}else if (res.equals("1")) {
+				n1++;
+				if (mk.equals("selected")) {
+					s1++;
+				}else if(mk.equals("un")){
+					u1++;
+				}
+			}else if (res.equals("2")) {
+				n2++;
+				if (mk.equals("selected")) {
+					s2++;
+				}else if(mk.equals("un")){
+					u2++;
+				}
+			}else if (res.equals("3")) {
+				n3++;
+				if (mk.equals("selected")) {
+					s3++;
+				}else if(mk.equals("un")){
+					u3++;
+				}
+			}
+    	 }
+    	 System.out.println(nn+" "+n0+" "+n1+" "+n2+" "+n3);
+//    	 System.out.println("s0: "+s0);
+//    	 System.out.println("u0: "+u0);
+//    	 System.out.println("s1: "+s1);
+//    	 System.out.println("u1: "+u1);
+    	 System.out.println(s0);
+    	 System.out.println(s1);
+    	 System.out.println(s2);
+    	 System.out.println(s3);
+    	 System.out.println(u0);
+    	 System.out.println(u1);
+    	 System.out.println(u2);
+    	 System.out.println(u3);
+    	 
+    	 System.out.println("0: "+  (float)s0/(float)(s0+u0));
+    	 System.out.println("1: "+  (float)s1/(float)(s1+u1));
+    	 System.out.println("1: "+  (float)s0/(float)(s0+s1));
+
+
+
+
+
+ 	    	   
+    	    	     	 
+    	 System.out.println("1: "+count2);
+//    	 System.out.println("2: "+markCl.size());
+
+	
+    }
+
+    static void buildFlag(){
+      	 try {
+   	    	FileReader fileReader = new FileReader("flag.txt");
+   	        BufferedReader bufferedReader = new BufferedReader(fileReader);
+   	        String line = null;
+   	        while ((line=bufferedReader.readLine())!= null) {
+   	        	count1++;
+   				markSe.add(line);
+   				
+   			}
+   		} catch (IOException e) {
+   			e.printStackTrace();
+   		}
+       }}
+
 
